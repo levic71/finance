9f5cd6cdcf51132be069be76b2b68934a7739273
Levic71 <vmlf71@gmail.com>
2021-02-17 15:30:53 +0100

    back

--- wrapper/head.php
+++ wrapper/head.php
@@ -122,7 +122,6 @@ window.onload = function() {
 		mm({action: 'dashboard'});
 	<? } ?>
 
-
 	var opts = { x_colors: ['#00525D', '#474554', '#FAF8FF', '#8F8D9E'], variance: 0, cell_size: 50, width: window.innerWidth, height: window.innerHeight };
 	var pattern = Trianglify(opts);
 	document.body.appendChild(pattern.canvas());
