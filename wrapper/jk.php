3f184ed3881015802de2ef4eb41c2600f06a81cb
Levic71 <vmlf71@gmail.com>
2021-04-16 16:03:29 +0200

    vfe

--- wrapper/jk.php
+++ wrapper/jk.php
@@ -7,16 +7,12 @@ ini_set("session.cookie_domain", ".jorkers.com");
 ini_set('session.cookie_secure', 1);
 ini_set('session.cookie_httponly', 1);
 ini_set('session.use_only_cookies', 1);
-ini_set('session.cookie_samesite', 'None');
 ini_set("url_rewriter.tags", "input=src");
 ini_set('arg_separator.output', '&amp;');
 
 // Nouvelle session
 session_cache_expire(60 * 60);
 session_start();
-
-// Pb sur safari
-session_set_cookie_params(0, "/");
 
 require_once "../include/sess_context.php";
 
