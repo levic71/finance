6f5d0055c5782a00ff42dadcf986e274e1fd7aba
Levic71 <vmlf71@gmail.com>
2022-03-15 09:54:04 +0100

    vfe

--- finance/include.php
+++ finance/include.php
@@ -231,7 +231,7 @@ class calc {
         $today = new DateTime(date("Y-m-d"));
 
         // Récupération et TRT des ordres passes
-        $req = "SELECT * FROM orders WHERE portfolio_id IN (".($portfolio['infos']['synthese'] == 1 ? $portfolio['infos']['all_ids'] : $infos['id']).") ORDER BY date, datetime ASC";
+        $req = "SELECT * FROM orders WHERE portfolio_id IN (".($portfolio['infos']['synthese'] == 1 ? $portfolio['infos']['all_ids'] : $infos['id']).") ORDER BY date, symbol, datetime ASC";
         $res = dbc::execSql($req);
         while($row = mysqli_fetch_assoc($res)) {
 
