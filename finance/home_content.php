9a448a38b2e671143a9e1802d4c1c5e00996c36a
Levic71 <vmlf71@gmail.com>
2022-07-04 16:08:04 +0200

    vfe

--- finance/home_content.php
+++ finance/home_content.php
@@ -13,6 +13,10 @@ foreach([''] as $key)
 
 $db = dbc::connect();
 
+$file_cache = 'cache/TMP_ALERTES.json';
+$ret = cacheData::readCacheData($file_cache);
+var_dump($ret);
+
 // SQL SCHEMA UPDATE
 // $ret = dbc::delColTable("trend_following", "quotes", "ALTER TABLE `portfolios` DROP `quotes`;");
 // $ret = dbc::addColTable("orders", "devise", "ALTER TABLE `orders` ADD `devise` VARCHAR(16) NOT NULL AFTER `price`;");
