5931ee4934e97ba686c62d0fd9e59e9987d8d75c
Levic71 <vmlf71@gmail.com>
2023-01-06 17:39:40 +0100

    vfe

--- finance/portfolio_graph.php
+++ finance/portfolio_graph.php
@@ -18,8 +18,6 @@ if (!$sess_context->isUserConnected()) {
 	exit(0);
 }
 
-// $data_ptf = [ 'days' => [ "0" ], 'valo' => [ "0" ], 'depot' => [ "0" ] ];
-
 // Recuperation des infos du portefeuille
 $req = "SELECT pv.*, p.name FROM portfolios p, portfolio_valo pv WHERE pv.portfolio_id=".$portfolio_id." AND p.id=pv.portfolio_id AND p.user_id=".$sess_context->getUserId();
 $res = dbc::execSql($req);
@@ -30,18 +28,19 @@ while ($row = mysqli_fetch_assoc($res)) {
 
     $data = json_decode($row['data']);
     // var_dump($data);
-    $data_ptf['days'][$row['date']]  = $row['date'];
-    $data_ptf['valo'][$row['date']]  = Round($data->valo_ptf);
-    $data_ptf['depot'][$row['date']] = Round($data->depot);
+    $data_ptf[$row['date']]['day']  = $row['date'];
+    $data_ptf[$row['date']]['valo']  = Round($data->valo_ptf);
+    $data_ptf[$row['date']]['depot_acc'] = Round($data->depot);
     $name = $row['name'];
 
 }
 
-// Ajout de la valo TR
+// Ajout de la valo temps reel
 $data_ptf_now = calc::aggregatePortfolioById($portfolio_id);
-$data_ptf['days'][]  = date("Y-m-d");
-$data_ptf['valo'][]  = Round($data_ptf_now['valo_ptf']);
-$data_ptf['depot'][] = Round($data_ptf_now['depot']);
+$data_ptf[date("Y-m-d")]['day']   = date("Y-m-d");
+$data_ptf[date("Y-m-d")]['valo']  = Round($data_ptf_now['valo_ptf']);
+$data_ptf[date("Y-m-d")]['depot_acc'] = Round($data_ptf_now['depot']);
+
 $my_portfolio  = $data_ptf_now['infos'];
 $lst_positions = $data_ptf_now['positions'];
 $lst_orders    = $data_ptf_now['orders'];
@@ -59,34 +58,21 @@ foreach($lst_orders as $key => $val) {
 
     if ($val['action'] == 1 || $val['action'] == -1) $nb_orders++;
 
+    $pricing = $val['price'] * $val['quantity'] * $val['taux_change'];
+
     if ($val['action'] == 1) {
-        $tab_orders['achat'][$val['date']] = (isset($tab_orders['achat'][$val['date']]) ? $tab_orders['achat'][$val['date']] : 0) + ($val['price'] * $val['quantity'] * $val['taux_change']);
+        $data_ptf[$val['date']]['achat'] = (isset($tab_orders[$val['date']]['achat']) ? $tab_orders[$val['date']]['achat'] : 0) + $pricing;
     } if ($val['action'] == 2) {
-        $tab_orders['depot'][$val['date']] = (isset($tab_orders['depot'][$val['date']]) ? $tab_orders['depot'][$val['date']] : 0) + ($val['price'] * $val['quantity'] * $val['taux_change']);
+        $data_ptf[$val['date']]['depot_j'] = (isset($tab_orders[$val['date']]['depot_j']) ? $tab_orders[$val['date']]['depot_j'] : 0) + $pricing;
     } if ($val['action'] == 4 || $val['action'] == 6) {
-        $tab_orders['dividende'][$val['date']] = (isset($tab_orders['dividende'][$val['date']]) ? $tab_orders['dividende'][$val['date']] : 0) + ($val['price'] * $val['quantity'] * $val['taux_change']);
+        $data_ptf[$val['date']]['dividende'] = (isset($tab_orders[$val['date']]['dividende']) ? $tab_orders[$val['date']]['dividende'] : 0) + $pricing;
     } if ($val['action'] == -1) {
-        $tab_orders['vente'][$val['date']] = (isset($tab_orders['vente'][$val['date']]) ? $tab_orders['vente'][$val['date']] : 0) + ($val['price'] * $val['quantity'] * $val['taux_change']);
+        $data_ptf[$val['date']]['vente'] = (isset($tab_orders[$val['date']]['vente']) ? $tab_orders[$val['date']]['vente'] : 0) + $pricing;
     } if ($val['action'] == -2) {
-        $tab_orders['retrait'][$val['date']] = (isset($tab_orders['retrait'][$val['date']]) ? $tab_orders['retrait'][$val['date']] : 0) + ($val['price'] * $val['quantity'] * $val['taux_change']);
+        $data_ptf[$val['date']]['retrait'] = (isset($tab_orders[$val['date']]['retrait']) ? $tab_orders[$val['date']]['retrait'] : 0) + $pricing;
     }
 
 }
-
-// Completion a zero pour les jours vide
-foreach($data_ptf["days"] as $key => $val) {
-    foreach([ 'achat', 'vente', 'depot', 'retrait', 'dividende'] as $key2 => $elt)
-        if (!isset($tab_orders[$elt][$val])) $tab_orders[$elt][$val] = 0;
-}
-foreach([ 'achat', 'vente', 'depot', 'retrait', 'dividende'] as $key2 => $elt) ksort($tab_orders[$elt]);
-
-// Completion a zero pour les jours vide
-foreach($tab_orders["days"] as $key => $val) {
-    foreach([ 'days', 'valo', 'depot'] as $key2 => $elt)
-        if (!isset($data_ptf["days"][$val])) $data_ptf[$elt][$val] = $elt == 'days' ? $val : "";
-}
-foreach([ 'days', 'valo', 'depot'] as $key2 => $elt)
-    ksort($data_ptf[$elt]);
 
 ?>
 
@@ -94,15 +80,46 @@ foreach([ 'days', 'valo', 'depot'] as $key2 => $elt)
 
 <div id="canvas_area" class="ui container inverted segment">
     <canvas id="portfolio_canvas" height="100"></canvas>
-    <canvas id="orders_canvas"    height="100"></canvas>
 </div>
 
 <script>
 
-var myChart1 = null;
-var myChart2 = null;
+var myChart = null;
 
-newDataset = function(mydata, mytype, yaxeid, mylabel, mycolor, bg, myfill, myborderwith = 0.5, mytension = 0.4, myradius = 0) {
+var mydata = [<?
+    $i = 1;
+    $count = count($data_ptf);
+    ksort($data_ptf);
+
+    foreach($data_ptf as $key => $val) {
+        echo sprintf("{ d: '%s', vl: %.2f, da: %.2f, ha: %.2f, dj: %.2f, dd: %.2f, vt: %.2f, rt: %.2f }%s",
+            $key,
+            isset($val["valo"])      ? $val["valo"]      : 0,
+            isset($val["depot_acc"]) ? $val["depot_acc"] : 0,
+            isset($val["achat"])     ? $val["achat"]     : 0,
+            isset($val["depot_j"])   ? $val["depot_j"]   : 0,
+            isset($val["dividende"]) ? $val["dividende"] : 0,
+            isset($val["vente"])     ? $val["vente"]     : 0,
+            isset($val["retrait"])   ? $val["retrait"]   : 0,
+            $i++ == $count ? '' : ','
+        );
+    }
+?>];
+
+var mydays = [<?
+    $i = 1;
+    $count = count($data_ptf);
+    ksort($data_ptf);
+
+    foreach($data_ptf as $key => $val) {
+        echo sprintf("'%s'%s",
+            $key,
+            $i++ == $count ? '' : ','
+        );
+    }
+?>];
+
+newDataset = function(mydata, mytype, yaxeid, yaxekey, mylabel, mycolor, bg, myfill, myborderwith = 0.5, mytension = 0.4, myradius = 0) {
 
     var ret = {
         type: mytype,
@@ -111,43 +128,35 @@ newDataset = function(mydata, mytype, yaxeid, mylabel, mycolor, bg, myfill, mybo
         borderColor: mycolor,
         borderWidth: myborderwith,
         yAxisID: yaxeid,
+        parsing: {
+            xAxisKey: 'd',
+            yAxisKey: yaxekey
+        },
         cubicInterpolationMode: 'monotone',
         tension: mytension,
         backgroundColor: bg,
         fill: myfill,
         pointRadius: myradius,
+        normalized: true
     };
 
     return ret;
 }
 
-getDatasetVals = function(label, type, vals, colr, bgcolr) {
-    return newDataset(vals, type, 'y', label, colr, bgcolr, true);
+getDatasetVals = function(label, type, vals, yaxekey, colr, bgcolr) {
+    return newDataset(vals, type, 'y', yaxekey, label, colr, bgcolr, true);
 }
 
-getDatasetVals2 = function(label, type, vals, colr, bgcolr, stack) {
-    var ds = newDataset(vals, type, 'y', label, colr, bgcolr, true);
+getDatasetVals2 = function(label, type, vals, yaxekey, colr, bgcolr, stack) {
+    var ds = newDataset(vals, type, 'y2', yaxekey, label, colr, bgcolr, true);
     ds.stack = stack;
     return ds;
 }
 
 var graphe_size_days = 0;
 
-// Ref Daily data
-var g1_days   = [<?= '"' . implode('","', $data_ptf["days"]) . '"' ?>];
-var g1_vals   = [<?= implode(',', $data_ptf["valo"])  ?>];
-var g2_vals   = [<?= implode(',', $data_ptf["depot"]) ?>];
-var g3_vals   = [<?= implode(',', $tab_orders["achat"]) ?>];
-var g4_vals   = [<?= implode(',', $tab_orders["vente"]) ?>];
-var g5_vals   = [<?= implode(',', $tab_orders["depot"]) ?>];
-var g6_vals   = [<?= implode(',', $tab_orders["retrait"])   ?>];
-var g7_vals   = [<?= implode(',', $tab_orders["dividende"]) ?>];
-
 var ctx1 = document.getElementById('portfolio_canvas').getContext('2d');
-el("portfolio_canvas").height = document.body.offsetWidth > 700 ? 80 : 200;
-
-var ctx2 = document.getElementById('orders_canvas').getContext('2d');
-el("orders_canvas").height = document.body.offsetWidth > 700 ? 40 : 100;
+el("portfolio_canvas").height = document.body.offsetWidth > 700 ? 140 : 300;
 
 update_graph_chart = function(c, ctx, opts, lbls, dtsts, plg) {
     if (c) c.destroy();
@@ -168,20 +177,15 @@ update_graph_chart = function(c, ctx, opts, lbls, dtsts, plg) {
 update_all_charts = function() {
 
     // Update Chart Portfolio
-    var datasets1 = [];
-    datasets1.push(getDatasetVals('D\u00e9pot', 'line', g2_vals, '<?= $sess_context->getSpectreColor(2) ?>', '<?= $sess_context->getSpectreColor(2, 0.15) ?>'));
-    datasets1.push(getDatasetVals('Valo',       'line', g1_vals, '<?= $sess_context->getSpectreColor(3) ?>', '<?= $sess_context->getSpectreColor(3, 0.3) ?>'));
-    myChart1 = update_graph_chart(myChart1, ctx1, options_Valo_Graphe, g1_days, datasets1, []);
-
-    // Update Chart Orders
-    var datasets2 = [];
-    datasets2.push(getDatasetVals2('Achat', 'bar', g3_vals,     'rgba(150, 238, 44, 1)', 'rgba(150, 238, 44, 1)', 'In'));
-    datasets2.push(getDatasetVals2('Vente', 'bar', g4_vals,     'rgba(236, 3, 59, 1)', 'rgba(236, 3, 59, 1)', 'Out'));
-    datasets2.push(getDatasetVals2('Depot', 'bar', g5_vals,     'rgba(3, 130, 236, 1)', 'rgba(3, 130, 236, 1)', 'In'));
-    datasets2.push(getDatasetVals2('Retrait', 'bar', g6_vals,   'rgba(238, 229, 44, 1)', 'rgba(238, 229, 44, 1)', 'Out'));
-    datasets2.push(getDatasetVals2('Dividende', 'bar', g7_vals, 'rgba(0, 236, 193, 1)', 'rgba(0, 236, 193, 1)', 'In'));
-    options_Orders_Graphe.scales['x'].ticks.display = true;
-    myChart2 = update_graph_chart(myChart2, ctx2, options_Orders_Graphe, g1_days, datasets2, []);
+    var ds= [];
+    ds.push(getDatasetVals('D\u00e9pot Acc', 'line', mydata, 'vl', '<?= $sess_context->getSpectreColor(2) ?>', '<?= $sess_context->getSpectreColor(2, 0.15) ?>'));
+    ds.push(getDatasetVals('Valo',           'line', mydata, 'da', '<?= $sess_context->getSpectreColor(3) ?>', '<?= $sess_context->getSpectreColor(3, 0.3) ?>'));
+    ds.push(getDatasetVals2('Achat',         'bar',  mydata, 'ha', 'rgba(150, 238, 44, 1)', 'rgba(150, 238, 44, 1)', 'In'));
+    ds.push(getDatasetVals2('Vente',         'bar',  mydata, 'vt', 'rgba(236, 3, 59, 1)',   'rgba(236, 3, 59, 1)',   'Out'));
+    ds.push(getDatasetVals2('D\u00e9pot J',  'bar',  mydata, 'dt', 'rgba(3, 130, 236, 1)',  'rgba(3, 130, 236, 1)',  'In'));
+    ds.push(getDatasetVals2('Retrait',       'bar',  mydata, 'rt', 'rgba(238, 229, 44, 1)', 'rgba(238, 229, 44, 1)', 'Out'));
+    ds.push(getDatasetVals2('Dividende',     'bar',  mydata, 'dd', 'rgba(0, 236, 193, 1)',  'rgba(0, 236, 193, 1)',  'In'));
+    myChart = update_graph_chart(myChart, ctx1, options_Valo_Graphe, null, ds, []);
 
 }
 
