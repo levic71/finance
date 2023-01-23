f16b5c472f0b460edb6fd80187469e295d3ab35c
Levic71 <vmlf71@gmail.com>
2023-01-23 17:06:14 +0100

    vfe

--- finance/stock_detail.php
+++ finance/stock_detail.php
@@ -757,6 +757,7 @@ if (!$readonly) {
             let j = beginAt;
             result.points.forEach(function(item) { tab_item[j]['reg'] = item[1]; tab_item[j]['r2'] = result.r2 + '/' + result.string; j++; });
 
+            console.log(beginAt);
             console.log(d_data_reg);
             console.log(result);
             console.log(tab_item);
