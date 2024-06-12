3a72fd1b0fa0f2c4c33f5e82895c445098cf33bf
Levic71 <vmlf71@gmail.com>
2024-06-12 18:23:44 +0200

    cron-job

--- finance/crontab.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -X PATCH -H 'Content-Type: application/json' -H 'Authorization: Bearer RtWJSAE3zWRkwYzX70uo4lfPzfJSjLr5r7udu8Poh2c=' -d '{"job":{"enabled":true}}' https://api.cron-job.org/jobs/4061357
-
